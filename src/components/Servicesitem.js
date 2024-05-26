756616ad8d3546592a4af6ef7fc73fb5c1e92b04
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-26 18:16:17 -0500

    notes

--- src/components/Servicesitem.js
+++ src/components/Servicesitem.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// ServicesItem component receives props including category, image, title, description, price, onAddToCart, and removeFromCart
 function ServicesItem({
   category,
   image,
@@ -9,32 +10,39 @@ function ServicesItem({
   onAddToCart,
   removeFromCart,
 }) {
-   const item = { category, image, title, description, price };
+  // Creating an 'item' object from received props
+  const item = { category, image, title, description, price };
+
   return (
     <div className="service-item">
+      {/* Displaying service item image */}
       <img
         src={image}
         alt={title}
         style={{ width: "100%", borderRadius: "4px" }}
       />
+      {/* Displaying service item title */}
       <h3>{title}</h3>
+      {/* Displaying service item description */}
       <p>{description}</p>
+      {/* Displaying service item price */}
       <p>${price}</p>
+      {/* Button to add item to cart */}
       <button
         className="add-btn"
         onClick={(event) => {
           event.preventDefault();
-          onAddToCart(item);
+          onAddToCart(item); // Call 'onAddToCart' function with 'item' object as argument
         }}
       >
         Add to cart
       </button>
-
+      {/* Button to remove item from cart */}
       <button
         className="remove-btn"
-        onClick={() => removeFromCart(item.title, item.id)}
+        onClick={() => removeFromCart(item.title, item.id)} // Call 'removeFromCart' function with title and id as arguments
       >
-        Remove 
+        Remove
       </button>
     </div>
   );
