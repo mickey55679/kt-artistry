94157893f6b8b2595c481cb7bf09de5f0f004a3b
Michaiah Bos <michaiahbos@yahoo.com>
2024-05-27 20:36:10 -0500

    wip

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,15 @@
+import familyImg from './images/Family.jpeg'
+
+
 function About() {
-  return (<div></div>);
+  return (
+    <div className="about-container">
+      <div className="column-one">
+        <img src={familyImg} alt="Family" />
+      </div>
+      <div className="column-one">Column 2</div>
+      <div className="column-one">Column 3</div>
+    </div>
+  );
 }
 export default About;
